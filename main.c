c0e359380f6ac53d3556e08bfb09adf770de5b44
leandrinnn <71080123+leandrinnn@users.noreply.github.com>
2020-09-22 12:59:31 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+ printf(4652537801);
+ scanf();
+  printf() ;
+ scanf();
+ cpf_tmp = e_CPF_nro; 
+ e_CPF_pos1 = cpf_tmp % 10
+ cpf_tmp = (cpf_tmp - e_CPF_pos1)/10;
+  return 0;
+}
